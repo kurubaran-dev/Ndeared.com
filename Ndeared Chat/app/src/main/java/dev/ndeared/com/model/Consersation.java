3d9007e0fd7984a73086a1edd2c14de6586c59da
kurubaran-dev <53267209+kurubaran-dev@users.noreply.github.com>
2019-08-11 11:18:56 +0530

    Ndeared Chat (Android Version)
    Simple.Secure.
    Real-Time Messaging.
    
    People can send voice memos, photos and videos to each other on Ndeared communicating without text. People needed to know they successfully connected their contacts to Ndeared and where to begin their conversations.

---
+++ Ndeared Chat/app/src/main/java/dev/ndeared/com/model/Consersation.java
@@ -0,0 +1,16 @@
+package dev.ndeared.com.model;
+
+import java.util.ArrayList;
+
+
+
+public class Consersation {
+    private ArrayList<Message> listMessageData;
+    public Consersation(){
+        listMessageData = new ArrayList<>();
+    }
+
+    public ArrayList<Message> getListMessageData() {
+        return listMessageData;
+    }
+}
---
+++ Ndeared Chat/app/src/main/java/dev/ndeared/com/model/Friend.java
@@ -0,0 +1,8 @@
+package dev.ndeared.com.model;
+
+
+
+public class Friend extends User{
+    public String id;
+    public String idRoom;
+}
